db2576b952df71b30a7b4d1b31f6ea8b98c1a786
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-06-21 12:53:28 -0500

    obteniedo con future then

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String title = "Hola a todos";
+
+  Future<String> getTitle() {
+    return Future.delayed(Duration(seconds: 3), () => "Los inocentes");
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(title, style: TextStyle(fontSize: 50)),
+            ElevatedButton(
+              onPressed: () {
+                getTitle().then((valor) {
+                  title = valor;
+                  setState(() {});
+                });
+              },
+              child: Text("Obtener título"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
