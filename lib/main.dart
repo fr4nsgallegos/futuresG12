6487a1baeb35d207d42581d9f94ce0ca96ea19fa
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-06-21 12:34:59 -0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:futuresg12/pages/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage(), debugShowMaterialGrid: false));
+}
